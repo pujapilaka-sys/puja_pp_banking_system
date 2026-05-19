7557d636ce6aea4fbffb714de0f525ea2f1958e5
pujapilaka-sys <pujapilaka@gmail.com>
2026-05-19 04:57:54 +0000

    Added

---
+++ puja/src/com_puja_model/tree.java
@@ -0,0 +1,11 @@
+package com_puja_model;
+
+public class tree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to our school");
+
+	}
+
+}
